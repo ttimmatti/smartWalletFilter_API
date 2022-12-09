bdd9f03b3f2e32a9f0694d7d5d99fea72d83b642
ttimmatti <ttimmatti@r7.4700>
2022-12-09 11:12:18 +0300

    day3

--- Program.cs
+++ Program.cs
@@ -14,9 +14,6 @@ class Program
     {
         DotNetEnv.Env.Load();
 
-        string ftm_api = Environment.GetEnvironmentVariable("FTM_API");
-
-
         bool development = true;
         if (!development)
         {
@@ -36,18 +33,7 @@ class Program
             app.Run();
         }
 
-        //SmartWallets.Get(File.ReadAllText("here.csv"), "10000", "5", "1000", "");
-
-        HttpClient httpClient = new HttpClient();
-
-        while(true)
-        {
-            HttpRequestMessage requestMessage = new HttpRequestMessage();
-            requestMessage.RequestUri = new Uri("");
-
-
-        }
-
+        SmartWallets.Get(File.ReadAllText("here.csv"), 1000, 5, 1000, "");
     }
 }
 
@@ -57,7 +43,7 @@ class Maps
     {
         //?balance=10000&txs=5&minswap=1000&token=<token address>
         app.MapPut("/upload",
-            async (HttpRequest request, string? balance, string? txs, string? minswap, string? token) =>
+            async (HttpRequest request, int? balance, int? txs, int? minswap, string? token) =>
             {
                 string fileContent = "";
                 using (var reader = new StreamReader(request.Body, System.Text.Encoding.UTF8))
@@ -79,17 +65,21 @@ class Maps
 
 class SmartWallets
 {
-    public static string Get(string csv, string balance, string numOfTxs, string minSwap, string token)
+    public static string Get(string csv, int? balance, int? numOfTxs, int? minSwap, string token)
     {
         //if value is null then we assign it its default value
-        balance = balance??"10000"; numOfTxs = numOfTxs??"5"; minSwap = minSwap??"1000"; token = token??"";
+        balance = balance??10000; numOfTxs = numOfTxs??5; minSwap = minSwap??1000; token = token??"";
+
+        //load networks
 
         //csv to list of TXs
         IList<Tx> list = Data.CsvToListOfTXs(csv);
         //filter for duplicates by txhash
-        list = list.GroupBy(el => el.Txhash).Select(el => el.First()).ToList<Tx>();
+        //list = list.GroupBy(el => el.Txhash).Select(el => el.First()).ToList<Tx>();
 
-        /*
+        /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        0. Determine the network of TXs
+
         1. Swap value > <minswap|1000$>
         If true:
             Check that the wallet is active: 
@@ -103,25 +93,41 @@ class SmartWallets
 
         //determine the network
         //What if the Api is down!!!
+        Explorer.DetermineNetwork(list);
 
         //1. SWAP VALUE > minSwap|1000$
+        // check for stables vs 1000
+        // need to hardcode:
+        // chain native tokens(5) and wrapped(5) and natives in the network, stables(usdt,busd,dai,usdn etc)
+        //
+        //list = list.Where(x => double.Parse(x.USDValueDayOfTx) >= minSwap).ToList<Tx>();
+        IList<string> walletsList = new List<string>();
+
+        // int j = 0;
+        // for (int i = 0; i < list.GroupBy(x => x.ContractAddress).Count(); i++)
+        // {
+        //     while (list.ElementAt(j).ContractAddress == list.ElementAt(i).ContractAddress)
+        //     {
+        //         //if ()
+
+        //         j++;
+        //     }
+        // }
+        //
         //
         //
 
-        //
-        //
-        //
 
 
-        //File.WriteAllText("here.json", JsonConvert.SerializeObject(list, Formatting.Indented));
+        File.WriteAllText("here.json", JsonConvert.SerializeObject(list, Formatting.Indented));
 
         //Tx tx = list.ElementAt(0);
         
         return ($"balance={balance}&txs={numOfTxs}&minswap={minSwap}&token={token}");
     }
-
-    
 }
+
+
 
 class Data
 {
@@ -131,6 +137,13 @@ class Data
         var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture);
 
         var records = csv.GetRecords<Tx>().ToList<Tx>();
+
+        for (int i = 0; i < records.Count(); i++)
+        {
+            string txUsd = records.ElementAt(i).USDValueDayOfTx;
+            if (txUsd.IndexOf(",") != -1)
+                records.ElementAt(i).USDValueDayOfTx = txUsd.Remove(txUsd.IndexOf(","), 1);
+        }
 
         return records;
     }
@@ -151,3 +164,101 @@ class Tx
 }
 
 //"Txhash","UnixTimestamp","DateTime","From","To","TokenValue","USDValueDayOfTx","ContractAddress","TokenName","TokenSymbol"
+
+class Stables
+{
+    // public static string[] All(string network)
+    // {
+
+    // }
+}
+
+class Network
+{
+    public string Name { get; set; }
+    public string Api { get; set; }
+    public string Key { get; set; }
+    public static IList<Network> AllNetworks()
+    {
+        return JsonConvert.DeserializeObject<List<Network>>(File.ReadAllText("networks.json"));
+    }
+    
+}
+
+class Explorer
+{
+    public static string DetermineNetwork(IList<Tx> listOfTxs)
+    {
+        IList<Network> listOfNetworks = Network.AllNetworks();
+
+        HttpClient httpClient = new HttpClient();
+        
+        //while? there still is a second variant(network)
+        for (int i = 0; i < 100; i++)
+        {
+            Tx tx = listOfTxs.ElementAt(i);
+
+            Console.WriteLine("".PadRight(5, '#'));
+
+            for (int j = 0; j < listOfNetworks.Count(); j++)
+            {
+                Network network = listOfNetworks.First(x => x.Name == "Polygon");
+
+                string status = GetResponseAsync(httpClient, network, tx).Result;
+
+                Console.WriteLine($"{j}Result for upper:" + status);
+            }
+        }
+
+        return "2";
+    }
+
+    private static async Task<string> GetResponseAsync(HttpClient httpClient, Network network, Tx tx)
+    {
+        TxReceiptResponse receiptResponse = null;
+        for (int i = 0; i < 2; i++)
+        {
+            bool success = true;
+            try
+            {       
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.RequestUri = new Uri(network.Api + "?module=transaction&action=gettxreceiptstatus&txhash=" +
+                tx.Txhash + "&apikey=" + network.Key);
+
+                var response = await httpClient.SendAsync(message);
+
+                var responseReader = response.Content.ReadAsStringAsync().Result;
+                receiptResponse = JsonConvert.DeserializeObject<TxReceiptResponse>(responseReader);
+            }
+            catch (Exception exc)
+            {
+                Thread.Sleep(3000);
+
+                success = false;
+                //SEND MESSAGE OR SMTHING //NEEDS TO BE REFACTORED LATER
+            }
+
+            if (success)
+                break;
+        }
+        
+        if (receiptResponse == null)
+        {
+            //also can be a message, to control availability
+            return "failed";
+        }
+
+        return receiptResponse.result.status;
+    }
+
+    private class TxReceiptResponse
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+        public Result result { get; set; }
+    }
+    private class Result
+    {
+        public string status { get; set; }
+    }
+}
